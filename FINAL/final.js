9780187de38015732b7524a10a381e4fc67ef8fd
Lily <57337789+lilyminkova@users.noreply.github.com>
2020-04-03 17:26:21 -0400

    Add files via upload

---
+++ FINAL/final.js
@@ -0,0 +1,31 @@
+// When the user clicks on <div>, open the popup
+function p1function() {
+  var popup = document.getElementById("p1");
+  popup.classList.toggle("show");
+}
+
+function p2function() {
+  var popup = document.getElementById("p2");
+  popup.classList.toggle("show");
+}
+
+
+function p3function() {
+  var popup = document.getElementById("p3");
+  popup.classList.toggle("show");
+}
+
+function p4function() {
+  var popup = document.getElementById("p4");
+  popup.classList.toggle("show");
+}
+
+function p5function() {
+  var popup = document.getElementById("p5");
+  popup.classList.toggle("show");
+}
+
+function p6function() {
+  var popup = document.getElementById("p6");
+  popup.classList.toggle("show");
+}
